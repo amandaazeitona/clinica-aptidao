b1ff1a9cdf9c8bd688092f4574e15d50528bb899
Fillype Nascimento <fillype.n@outlook.com>
2017-12-21 13:41:09 -0200

    fazendo navbar e o header

--- site/config/routes.rb
+++ site/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :sections
   devise_for :admins, path: 'auth'
+  get 'creatives/index'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
