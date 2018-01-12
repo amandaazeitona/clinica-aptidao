e9fc7ac89c54bfcfa6d84138a1988456f771d9ca
Fillype Nascimento <fillype.n@outlook.com>
2018-01-12 16:16:26 -0200

    terminada seção "sobre"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
 //= require creative/manifest
 //= require turbolinks
+//= require_tree .
