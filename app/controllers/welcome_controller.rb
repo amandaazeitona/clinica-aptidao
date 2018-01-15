cdedba3a52081de26b84d454a5cad76dd7f0a568
TheKipmaster <fkipman@hotmail.com>
2018-01-15 21:23:11 -0200

    Back-e-front-end da tabela de preços editável implementados

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @prices = Price.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :prices
+
+  # match 'prices/all/edit' => 'prices#edit_all', :as => :edit_all, :via => :get
+  get 'prices/all/edit', to: 'prices#edit_all', as: 'edit_all'
+  put 'prices/all', to: 'prices#update_all', as: 'update_all'
+  get 'prices', to: 'prices#index'
+
   devise_for :admins, path: 'auth'
   get 'creatives/index'
 
