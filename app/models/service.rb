0255d4d0c5ae93a7344fcaa02fc28383dcf15211
TheKipmaster <fkipman@hotmail.com>
2018-03-07 20:01:13 -0300

    Cria método de Serviços para facilitar exibição na view

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,9 @@ class Service < ApplicationRecord
   belongs_to :macro_service, class_name: "Service"
 
   accepts_nested_attributes_for :micro_services
+
+  # orders micro_services by id (because heroku) and drops the first
+  def normalize
+    self.micro_services.sort_by{ |a| a[:id] }.drop(1)
+  end
 end
